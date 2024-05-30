bfd7266ceed7d85ce6238d14f0766cad4268451d
Taehwan Kim <ehwank98@gmail.com>
2024-05-30 22:50:02 +0900

    fix Repeat Vec Output Merge was not implemented for all Tuple

--- rusty_parser/src/wrapper/vecmerge.rs
+++ rusty_parser/src/wrapper/vecmerge.rs
@@ -54,3 +54,151 @@ impl<T0, T1> VectorOutputSpecialize for (T0, T1) {
         output.0.push(self);
     }
 }
+
+impl<T0, T1, T2> VectorOutputSpecialize for (T0, T1, T2) {
+    type Output = (Vec<(T0, T1, T2)>,);
+
+    fn new_output() -> Self::Output {
+        (Vec::new(),)
+    }
+    fn reserve(output: &mut Self::Output, reserve_size: usize) {
+        output.0.reserve(reserve_size);
+    }
+    fn push_this_to_output(self, output: &mut Self::Output) {
+        output.0.push(self);
+    }
+}
+
+impl<T0, T1, T2, T3> VectorOutputSpecialize for (T0, T1, T2, T3) {
+    type Output = (Vec<(T0, T1, T2, T3)>,);
+
+    fn new_output() -> Self::Output {
+        (Vec::new(),)
+    }
+    fn reserve(output: &mut Self::Output, reserve_size: usize) {
+        output.0.reserve(reserve_size);
+    }
+    fn push_this_to_output(self, output: &mut Self::Output) {
+        output.0.push(self);
+    }
+}
+
+impl<T0, T1, T2, T3, T4> VectorOutputSpecialize for (T0, T1, T2, T3, T4) {
+    type Output = (Vec<(T0, T1, T2, T3, T4)>,);
+
+    fn new_output() -> Self::Output {
+        (Vec::new(),)
+    }
+    fn reserve(output: &mut Self::Output, reserve_size: usize) {
+        output.0.reserve(reserve_size);
+    }
+    fn push_this_to_output(self, output: &mut Self::Output) {
+        output.0.push(self);
+    }
+}
+
+impl<T0, T1, T2, T3, T4, T5> VectorOutputSpecialize for (T0, T1, T2, T3, T4, T5) {
+    type Output = (Vec<(T0, T1, T2, T3, T4, T5)>,);
+
+    fn new_output() -> Self::Output {
+        (Vec::new(),)
+    }
+    fn reserve(output: &mut Self::Output, reserve_size: usize) {
+        output.0.reserve(reserve_size);
+    }
+    fn push_this_to_output(self, output: &mut Self::Output) {
+        output.0.push(self);
+    }
+}
+
+impl<T0, T1, T2, T3, T4, T5, T6> VectorOutputSpecialize for (T0, T1, T2, T3, T4, T5, T6) {
+    type Output = (Vec<(T0, T1, T2, T3, T4, T5, T6)>,);
+
+    fn new_output() -> Self::Output {
+        (Vec::new(),)
+    }
+    fn reserve(output: &mut Self::Output, reserve_size: usize) {
+        output.0.reserve(reserve_size);
+    }
+    fn push_this_to_output(self, output: &mut Self::Output) {
+        output.0.push(self);
+    }
+}
+
+impl<T0, T1, T2, T3, T4, T5, T6, T7> VectorOutputSpecialize for (T0, T1, T2, T3, T4, T5, T6, T7) {
+    type Output = (Vec<(T0, T1, T2, T3, T4, T5, T6, T7)>,);
+
+    fn new_output() -> Self::Output {
+        (Vec::new(),)
+    }
+    fn reserve(output: &mut Self::Output, reserve_size: usize) {
+        output.0.reserve(reserve_size);
+    }
+    fn push_this_to_output(self, output: &mut Self::Output) {
+        output.0.push(self);
+    }
+}
+
+impl<T0, T1, T2, T3, T4, T5, T6, T7, T8> VectorOutputSpecialize
+    for (T0, T1, T2, T3, T4, T5, T6, T7, T8)
+{
+    type Output = (Vec<(T0, T1, T2, T3, T4, T5, T6, T7, T8)>,);
+
+    fn new_output() -> Self::Output {
+        (Vec::new(),)
+    }
+    fn reserve(output: &mut Self::Output, reserve_size: usize) {
+        output.0.reserve(reserve_size);
+    }
+    fn push_this_to_output(self, output: &mut Self::Output) {
+        output.0.push(self);
+    }
+}
+
+impl<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9> VectorOutputSpecialize
+    for (T0, T1, T2, T3, T4, T5, T6, T7, T8, T9)
+{
+    type Output = (Vec<(T0, T1, T2, T3, T4, T5, T6, T7, T8, T9)>,);
+
+    fn new_output() -> Self::Output {
+        (Vec::new(),)
+    }
+    fn reserve(output: &mut Self::Output, reserve_size: usize) {
+        output.0.reserve(reserve_size);
+    }
+    fn push_this_to_output(self, output: &mut Self::Output) {
+        output.0.push(self);
+    }
+}
+
+impl<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> VectorOutputSpecialize
+    for (T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10)
+{
+    type Output = (Vec<(T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10)>,);
+
+    fn new_output() -> Self::Output {
+        (Vec::new(),)
+    }
+    fn reserve(output: &mut Self::Output, reserve_size: usize) {
+        output.0.reserve(reserve_size);
+    }
+    fn push_this_to_output(self, output: &mut Self::Output) {
+        output.0.push(self);
+    }
+}
+
+impl<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> VectorOutputSpecialize
+    for (T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11)
+{
+    type Output = (Vec<(T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11)>,);
+
+    fn new_output() -> Self::Output {
+        (Vec::new(),)
+    }
+    fn reserve(output: &mut Self::Output, reserve_size: usize) {
+        output.0.reserve(reserve_size);
+    }
+    fn push_this_to_output(self, output: &mut Self::Output) {
+        output.0.push(self);
+    }
+}
