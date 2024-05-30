a5ea8cbad0efa8bbcc0890579673d4fe85dd0aeb
ehwan <ehwank98@gmail.com>
2024-05-30 12:59:28 +0900

    box

--- rusty_parser/src/wrapper/boxed.rs
+++ rusty_parser/src/wrapper/boxed.rs
@@ -1,22 +1,66 @@
 use std::boxed::Box;
+use std::ops::Deref;
+use std::ops::DerefMut;
 
 use crate::core::iterator_bound::InputIteratorTrait;
 use crate::core::parser::Parser;
 use crate::core::result::ParseResult;
 
-impl<ParserType, It> Parser<It> for Box<ParserType>
+pub struct BoxedParser<ParserType, It>
 where
-    ParserType: Parser<It>,
     It: InputIteratorTrait,
+    ParserType: Parser<It> + ?Sized,
 {
-    type Output = ParserType::Output;
+    parser: std::boxed::Box<ParserType>,
+    _phantom: std::marker::PhantomData<It>,
+}
+
+impl<ParserType, It> BoxedParser<ParserType, It>
+where
+    It: InputIteratorTrait,
+    ParserType: Parser<It> + ?Sized,
+{
+    pub fn new(parser: Box<ParserType>) -> Self {
+        Self {
+            parser: parser,
+            _phantom: std::marker::PhantomData,
+        }
+    }
+}
+impl<ParserType, It> Parser<It> for BoxedParser<ParserType, It>
+where
+    It: InputIteratorTrait,
+    ParserType: Parser<It> + ?Sized,
+{
+    type Output = <ParserType as Parser<It>>::Output;
 
     fn parse(&self, it: It) -> ParseResult<Self::Output, It> {
-        self.as_ref().parse(it)
+        self.parser.parse(it)
     }
 
     fn match_pattern(&self, it: It) -> ParseResult<(), It> {
-        self.as_ref().match_pattern(it)
+        self.parser.match_pattern(it)
+    }
+}
+
+impl<ParserType, It> Deref for BoxedParser<ParserType, It>
+where
+    It: InputIteratorTrait,
+    ParserType: Parser<It>,
+{
+    type Target = ParserType;
+
+    fn deref(&self) -> &Self::Target {
+        &self.parser
+    }
+}
+impl<ParserType, It> DerefMut for BoxedParser<ParserType, It>
+where
+    It: InputIteratorTrait,
+    ParserType: Parser<It>,
+{
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.parser
     }
 }
 
@@ -32,14 +76,17 @@ mod test {
         let a_parser = SingleEqualParser::new('a');
 
         let str = "1a2b3c4d5e6f7g8h9i0j";
-        let mut boxed: Box<dyn Parser<_, Output = (char,)>> = Box::new(digit_parser);
+        let mut boxed: BoxedParser<
+            dyn Parser<std::str::Chars<'_>, Output = (char,)>,
+            std::str::Chars<'_>,
+        > = BoxedParser::new(Box::new(digit_parser));
         let res = boxed.parse(str.chars());
         let rest: String = res.it.clone().collect();
         assert_eq!(res.output, Some(('1',)));
         assert_eq!(rest, "a2b3c4d5e6f7g8h9i0j");
 
         // set another parser to same variable
-        boxed = Box::new(a_parser);
+        boxed.parser = Box::new(a_parser);
         let res = boxed.parse(res.it);
         let rest: String = res.it.collect();
         assert_eq!(res.output, Some(('a',)));
